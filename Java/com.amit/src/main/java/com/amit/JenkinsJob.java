c236b07b9212c39569e8e6967edc3fc278728a77
amitkesharigithub <akkeshari1989@gmail.com>
2019-04-16 09:29:04 +0530

    Update JenkinsJob.java

--- Java/com.amit/src/main/java/com/amit/JenkinsJob.java
+++ Java/com.amit/src/main/java/com/amit/JenkinsJob.java
@@ -6,6 +6,7 @@ public class JenkinsJob {
 		System.out.println("hi amit");
 		System.out.println("hi amit");
 		System.out.println("Arise Awake keepworking till goal is achieved");
+		System.out.println("Doing work is greatest satisfaction");
 	}
 
 }
